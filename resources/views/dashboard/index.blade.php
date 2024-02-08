cf14a31425b24787f7589f434fdcbd36643f9805
kaungpyaesoneaunggic <kaungpyaesoneaung@gicjp.com>
2024-02-08 22:28:17 +0630

    Label Changed :)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,7 +4,7 @@
   
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>AdminLTE 3 | Dashboard</title>
+<title>Ticketing | Dashboard</title>
 
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -66,7 +66,7 @@
     <!-- Brand Logo -->
     <a class="brand-link">
       <img src={{ asset("dist/img/AdminLTELogo.png")}} alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">GIC Shopping</span>
+      <span class="brand-text font-weight-light">Happy Ticketing</span>
     </a>
 
     <!-- Sidebar -->
